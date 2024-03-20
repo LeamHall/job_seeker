38357e82cc422c43a445aef4c92ffd46c5f175b3
Leam Hall <leamhall@gmail.com>
2024-03-19 20:42:23 -0500

    Cleaned up cash_flow a little, still needs work.

--- cash_flow.py
+++ cash_flow.py
@@ -6,19 +6,16 @@
 # author  :  Leam Hall
 # desc    :  Trying to figure out how long we can last without a job.
 
-# TODO:
-#   Add an "annual expenses" argument, and pay 1/12 per month.
-#   pytest
-#   mypy
-#   flake8 and black
 
+""" Given integers for monthly expenses and income, along with current
+    savings and annual expenses, show how many months of bills can be paid.
+"""
 
 from argparse import ArgumentParser
 
-
-def per_month(out, income, savings):
-    return savings + income - out
-
+def per_month(out, income, savings, annual):
+    """ Returns an int of how much is left in monthly savings. """
+    return int(savings + income - out - (annual / 12))
 
 parser = ArgumentParser()
 parser.add_argument("-o", "--out", help="Money going out", default=1, type=int)
@@ -26,16 +23,27 @@ parser.add_argument("-s", "--savings", help="Savings", default=0, type=int)
 parser.add_argument(
     "-i", "--income", help="Money coming in", default=0, type=int
 )
+parser.add_argument("-a", "--annual", help="Annual expenses", default=0, type=int)
 args = parser.parse_args()
 
+MAX_TEST = 24
 months = 0
 have_enough = True
 savings = args.savings
 
 while have_enough:
-    savings = per_month(args.out, args.income, savings)
+    if months > MAX_TEST:
+        break
+    savings = per_month(args.out, args.income, savings, args.annual)
     if savings < args.out:
         have_enough = False
-    months += 1
+    else:
+        months += 1
 
-print("You have enough for {} months.".format(months))
+month_word = "months"
+if months == 1:
+    month_word = "month"
+elif months >= MAX_TEST:
+    months = "more than {}".format(MAX_TEST)
+
+print("You have enough for {} {}.".format(months, month_word))
